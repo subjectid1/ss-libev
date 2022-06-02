e8d3c2e2bdd9c2e51b1022d5baa9ad5ef46752ca
ghcdn <48314862+YulinChan@users.noreply.github.com>
2022-06-02 22:35:12 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 apk update && apk upgrade
 plugin='v''2ra''y-plu''gin'
+mode='websocket'
 wget https://dl.lamp.sh/files/${plugin}_linux_amd64 -qO /usr/local/bin/${plugin}
 chmod a+x /usr/local/bin/${plugin}
-ss-server -s 0.0.0.0 -p ${PORT} -k ${PASSWORD} --plugin ${plugin} --plugin-opts "server"
+ss-server -s 0.0.0.0 -p ${PORT} -k ${PASSWORD} --plugin ${plugin} --plugin-opts "server;mode=${mode}"
