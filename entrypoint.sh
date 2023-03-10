58e10b2f8361e817cd49cebed76052a32ab7ed59
subjectid1 <subjectid1@yandex.com>
2023-03-10 15:42:02 +0800

    Signed-off-by: subjectid1 <subjectid1@yandex.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@ apk update && apk upgrade
 plugin='v''2ra''y-plu''gin'
 wget https://dl.lamp.sh/files/${plugin}_linux_amd64 -qO /usr/local/bin/${plugin}
 chmod a+x /usr/local/bin/${plugin}
-ss-server -s sfsf-qhzas.b4a.run -p 443 -k 170826 -m chacha20-ietf-poly1305 --plugin ${plugin} --plugin-opts "server"
+ss-server -s 127.0.0.1 -p 443 -k 170826 -m chacha20-ietf-poly1305 --plugin ${plugin} --plugin-opts "server"
