288c1fd653d95ade7697a169a6fb2c2d340d32ef
subjectid1 <subjectid1@yandex.com>
2023-03-11 11:28:42 +0800

    Signed-off-by: subjectid1 <subjectid1@yandex.com>

--- entrypoint - 副本.sh
+++
@@ -1,5 +0,0 @@
-apk update && apk upgrade
-plugin='v''2ra''y-plu''gin'
-wget https://dl.lamp.sh/files/${plugin}_linux_amd64 -qO /usr/local/bin/${plugin}
-chmod a+x /usr/local/bin/${plugin}
-ss-server -s 0.0.0.0 -p 443 -k 170826 -m chacha20-ietf-poly1305 --plugin ${plugin} --plugin-opts "server"
